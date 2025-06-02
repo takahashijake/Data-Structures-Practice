d72476bd60a70e0144fd5ce8864a8f7ddab85e1f
takahashijake <jtakahashi@ucmerced.edu>
2025-06-01 20:24:16 -0700

    current progress formatting graph into cleaner files

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,8 +1,9 @@
 #include <iostream> 
+#include "helper.h"
 
-class BST{
+class BST : public HelperFunctions{
 
-    private:
+    public:
         struct Node{
             Node* parent;
             Node* left;
@@ -10,144 +11,8 @@ class BST{
             int value;
         };
 
-        Node* root; 
-
-        void addNodeHelper(Node* currentNode, Node* addNode){
-            if (addNode->value > currentNode->value){
-                if (currentNode->right == nullptr){
-                    addNode->parent = currentNode;
-                    currentNode->right = addNode;
-                }
-                else{
-                    addNodeHelper(currentNode->right, addNode);
-                }
-            }
-            else if (addNode->value <= currentNode->value){
-                if (currentNode->left == nullptr){
-                    addNode->parent = currentNode;
-                    currentNode->left = addNode;
-                }
-                else{
-                    addNodeHelper(currentNode->left, addNode);
-                }
-            }
-        }
         
-        void inOrderTraversalHelper(Node* currentNode){
-            if (currentNode != nullptr){
-                inOrderTraversalHelper(currentNode->left);
-                std::cout << currentNode->value << " ";
-                inOrderTraversalHelper(currentNode->right);
-            }
-        }
-
-        void deleteBST(Node* rootNode){
-            
-            if (rootNode != nullptr){
-                deleteBST(rootNode->left);
-                deleteBST(rootNode->right);
-                delete rootNode;
-            }
-        }
-
-        void searchHelper(Node* root, int value){
-            if (root->value == value){
-                std::cout << "Value has been found" << std::endl;
-            }
-            else if (value > root->value){
-                if (root->right == nullptr){
-                    std::cout << "Value has not been found!" << std::endl;
-                    return;
-                }
-                else if (root->right != nullptr){
-                    searchHelper(root->right, value);
-                }
-            }
-            else if (value < root->value){
-                if (root->left == nullptr){
-                    std::cout << "Value has not been found!" << std::endl;
-                }
-                else if (root->right != nullptr){
-                    searchHelper(root->left, value);
-                }
-            }
-        }
-
-        Node* deleteNodeSearch(Node* current, int value){
-            if (current == nullptr){
-                return nullptr;
-            }
-            else if (current->value == value){
-                return current;
-            }
-            else if (value > current->value){
-                if (current->right == nullptr){
-                    return nullptr;
-                }
-                else if (current->right != nullptr){
-                    return deleteNodeSearch(current->right, value);
-                }
-            }
-            else if (value <= current->value){
-                if (current->left == nullptr){
-                    return nullptr;
-                }
-                else if (current->left != nullptr){
-                    return deleteNodeSearch(current->left, value);
-                }
-            }
-            return nullptr;
-        }
-
-        Node* findSuccessor(Node* rightDeleteNode){
-            while (rightDeleteNode->left != nullptr){
-                findSuccessor(rightDeleteNode->left);
-            }
-            return rightDeleteNode->left;
-        }
-
-        void swapNodes(Node* firstNode, Node* secondNode){
-
-            //The node I want to swap the secondNode with is the root
-            if (firstNode == root){
-                //Because the firstNode is the root, the secondNode is guaranteed to have a parent 
-                if (secondNode->parent->left == secondNode){
-                    Node* temp = secondNode;
-                    secondNode->parent->left = nullptr;
-                    secondNode->parent = nullptr;
-                    secondNode->left->parent = nullptr;
-                    secondNode->right->parent = nullptr;
-
-                    secondNode = firstNode;
-
-                    secondNode->parent->left = temp->parent->left;
-                    secondNode->parent = temp->parent;
-                    secondNode->left->parent = temp->left->parent;
-                    secondNode->right->parent = temp->right->parent;
-    
-
-                    /*Node* secondTemp = firstNode;
-                    firstNode = temp;
-                    firstNode->parent = nullptr;
-                    firstNode->left = secondTemp->left;
-                    firstNode->right = secondTemp->right;
-
-                    delete temp*/
-                    delete temp;
-                }
-            
-                else if (secondNode->parent->right == secondNode){
-                    Node* temp = secondNode;
-                    secondNode = firstNode;
-                    secondNode->parent->right = temp->parent->right;
-                    secondNode->right = temp->right;
-                    secondNode->left = temp->left;
-                    firstNode = temp;
-                    delete temp;
-                }
-            }
-        }
-
+        Node* root; 
 
     public:
 
@@ -244,7 +109,7 @@ class BST{
             std::cout << "Value of firstNode is: " << firstNode->value << std::endl;
             std::cout << "Value of secondNode is: " << secondNode ->value << std::endl;
 
-            swapNodes(firstNode, secondNode);
+            //HelperFunctions::swapNodes(firstNode, secondNode);
         }
     
        ~BST(){
--- Graph/helper.h
+++ Graph/helper.h
@@ -1,5 +1,18 @@
-#ifndef "HELPER_H"
-#define "HELPER_H"
+#ifndef HELPER_H
+#define HELPER_H
+#include "BST.h"
 
+class HelperFunctions{
+
+    protected:
+
+        static void addNodeHelper(BST::Node* currentNode, BST::Node* addNode);
+        static void inOrderTraversalHelper(BST::Node* currentNode);
+        static void deleteBST(BST::Node* currentNode);
+        static void searchHelper(BST::Node* root, int value);
+        static BST::Node* deleteNodeSearch(BST::Node* current, int value);
+        static BST::Node* findSuccessor(BST::Node* rightDeleteNode);
+
+};
 
 #endif
