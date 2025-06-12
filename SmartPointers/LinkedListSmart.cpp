2a3bddc8d428429fd1dcf53d9c06b30a7cc864a0
takahashijake <jtakahashi@ucmerced.edu>
2025-06-11 18:58:45 -0700

    Fixed minor bug with addNode

--- SmartPointers/LinkedListSmart.cpp
+++ SmartPointers/LinkedListSmart.cpp
@@ -26,6 +26,7 @@ class LinkedList{
             newNode->next = nullptr;
             if (root == nullptr){
                 root = std::move(newNode);
+                tail = root.get();
             }
             else{
                 tail->next = std::move(newNode);
@@ -34,11 +35,25 @@ class LinkedList{
         }
 
         void printList() const {
-            
+            if (root == nullptr){
+                throw std::logic_error("Cannot print an empty list!");
+            }
+            Node* current = root.get();
+            while (current != nullptr){
+                std::cout << current->value << " ";
+                current = current->next.get();
+            }
+            std::cout << std::endl;
         }
 };
 
 int main(){
+
+    LinkedList<int> myList;
+    myList.addNode(1);
+    myList.addNode(2);
+    myList.addNode(3);
+    myList.printList();
 
     return 0;
 }
