78c9efa1e1aeecfe48d7408b0cd78158bb443eb3
takahashijake <jtakahashi@ucmerced.edu>
2025-06-12 23:05:48 -0700

    smart pointer linked list progress

--- SmartPointers/LinkedListSmart.cpp
+++ SmartPointers/LinkedListSmart.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <iostream>
 
+//Implementations are all made with the assumption that all elements are unique 
 template <typename T>
 class LinkedList{
 
@@ -12,6 +13,17 @@ class LinkedList{
 
         std::unique_ptr<Node> root;
         Node* tail;
+
+        Node* nodeSearchHelper(T value){
+            Node* current = root.get();
+            while (current != nullptr){
+                if (current->value == value){
+                    return current;
+                }
+                current = current->next.get();
+            }
+            return nullptr;
+        }
 
     public:
         
@@ -36,7 +48,7 @@ class LinkedList{
 
         void printList() const {
             if (root == nullptr){
-                throw std::logic_error("Cannot print an empty list!");
+                std::cout << "List is empty!" << std::endl;
             }
             Node* current = root.get();
             while (current != nullptr){
@@ -45,15 +57,33 @@ class LinkedList{
             }
             std::cout << std::endl;
         }
+
+        void deleteNode(T value){
+            Node* deleteNode = nodeSearchHelper(value);
+            if (deleteNode == nullptr){
+                std::cout << "List is empty!" << std::endl;
+            }
+            if (root->value == value){
+                std::unique_ptr<Node> rootDelete = std::move(root);
+                root = std::move(rootDelete->next);
+                rootDelete->next = nullptr;
+            }
+            else if (tail->value == value){
+                if (root->value == tail->value){
+                    std::unique_ptr<Node> tailDelete = std::move(root);
+                    root = nullptr;
+                }
+            }
+        }
 };
 
 int main(){
 
     LinkedList<int> myList;
     myList.addNode(1);
-    myList.addNode(2);
-    myList.addNode(3);
     myList.printList();
-
+    myList.deleteNode(1);
+    myList.printList();
     return 0;
+
 }
