f8362d01a7f68f401c20ad9b8aebf9415b14213b
takahashijake <jtakahashi@ucmerced.edu>
2025-06-16 00:21:53 -0700

    Added addNode function

--- SmartPointers/BST/BSTUnique.cpp
+++ SmartPointers/BST/BSTUnique.cpp
@@ -17,19 +17,67 @@ class BST{
             }
         };
 
-        Node* root;
+        std::shared_ptr<Node> root;
 
+        Node* nodeSearch(T value){
+            Node* current = root;
+            while (current != nullptr){
+                if (current->value == value){
+                    return current;
+                }
+                current = current->next.get();
+            }
+            return nullptr;
+        }
 
+        void inOrderTraversalHelper(const std::shared_ptr<Node>& current){
+            if (current != nullptr){
+                inOrderTraversalHelper(current->leftNode);
+                std::cout << current->value << " ";
+                inOrderTraversalHelper(current->rightNode);
+            }
+        }
 
+        void addNodeHelper(const std::shared_ptr<Node>& addNode, const std::shared_ptr<Node>& currentNode){
+            if (addNode->value > currentNode->value){
+                if (currentNode->rightNode == nullptr){
+                    currentNode->rightNode = std::move(addNode);
+                    return;
+                }
+                else{
+                    addNodeHelper(addNode, currentNode->rightNode);
+                }
+            }
+            else if (addNode->value < currentNode->value){
+                if (currentNode->leftNode == nullptr){
+                    currentNode->leftNode = std::move(addNode);
+                    return;
+                }
+                else{
+                    addNodeHelper(addNode,currentNode->leftNode);
+                }
+            }
+        }
     public:
 
         BST(){
-            root == nullptr;
+            root = nullptr;
             std::cout << "BST successfully initalized!" << std::endl;
         }
 
-        void printList(T value){
-            
+        void addNode(T value){
+            std::shared_ptr<Node> newNode = std::make_shared<Node>(nullptr, nullptr, value);
+            if (root == nullptr){
+                root = std::move(newNode);
+            }
+            else{
+                addNodeHelper(newNode, root);
+            }
+        }
+
+        void inOrderTraversal(){
+            inOrderTraversalHelper(root);
+            std::cout << std::endl;
         }
         ~BST(){
             std::cout << "BST successfully deleted!" << std::endl;
@@ -39,5 +87,11 @@ class BST{
 
 int main(){
 
+    BST<int> myGraph;
+
+    myGraph.addNode(1);
+    myGraph.addNode(2);
+    myGraph.addNode(3);
+    myGraph.inOrderTraversal();
     return 0;
 }
